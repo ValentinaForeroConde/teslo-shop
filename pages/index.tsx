27a1b79fd47742c5e03ccc4720313da8cf9aaf20
ValentinaForeroConde <valeforero98@gmail.com>
2022-10-06 20:23:19 -0500

    chore: page Layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,21 @@
+import { Typography } from "@mui/material";
 import type { NextPage } from "next";
+import { ShopLayout } from "../components/layouts";
 
 const Home: NextPage = () => {
-  return <h1>TESLO SHOP</h1>;
+  return (
+    <ShopLayout
+      title={"Teslo Shop - Home"}
+      pageDescription={"Find the best products of Teslo HERE!"}
+    >
+      <Typography variant="h1" component={"h1"}>
+        Shop
+      </Typography>
+      <Typography variant="h2" sx={{ mb: 1 }}>
+        All the products
+      </Typography>
+    </ShopLayout>
+  );
 };
 
 export default Home;
