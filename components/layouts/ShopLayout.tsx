e2f06ba70b391cc2a6059db64a8549f08b248a51
ValentinaForeroConde <valeforero98@gmail.com>
2022-10-09 17:50:55 -0500

    chore: initial sidemenu

--- components/layouts/ShopLayout.tsx
+++ components/layouts/ShopLayout.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import Head from "next/head";
 import { Navbar } from "../ui";
+import { SideMenu } from "../ui/SideMenu";
 
 interface Props {
   title: string;
@@ -26,7 +27,7 @@ export const ShopLayout: FC<Props> = ({
       </Head>
 
       <Navbar />
-      {/* TODO: side bar */}
+      <SideMenu />
 
       <main
         style={{ margin: "80px auto", maxWidth: "1440px", padding: "0px 30px" }}
